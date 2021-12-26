490e3ece422acf941ee6db240801df638ed3d6f8
Nikolay Yakubitskiy <steseagal87@gmail.com>
2021-12-27 02:30:40 +0300

    fixes for backend and frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080/v1",
+    baseURL: process.env.VUE_APP_ROOT_API,
     headers: {
         "Content-Type": "application/json",
     },
