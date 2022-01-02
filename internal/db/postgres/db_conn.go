08229495c46d227908bdfa643695dd5699d2addc
reddaemon <steseagal87@gmail.com>
2022-01-02 13:41:36 +0300

    Update postgres and redis connections for prod environment (#10)
    * added helm chart
    
    * fixes for backend and frontend
    
    * fix cookie problem
    
    * update postgres and redis connections for prod env

--- internal/db/postgres/db_conn.go
+++ internal/db/postgres/db_conn.go
@@ -12,28 +12,56 @@ import (
 )
 
 func NewPsqlDb(c *config.Config) (*pgxpool.Pool, error) {
-	connStr := fmt.Sprintf("%s://%s:%s@%s:%s/%s?sslmode=disable&connect_timeout=%d",
-		"postgres",
-		url.QueryEscape(c.Postgres.PostgresqlUser),
-		url.QueryEscape(c.Postgres.PostgresqlPassword),
-		c.Postgres.PostgresqlHost,
-		c.Postgres.PostgresqlPort,
-		c.Postgres.PostgresqlDbname,
-		5)
+	if os.Getenv("ENV") == "PRODUCTION" {
+		connStr := fmt.Sprintf("%s://%s:%s@%s:%s/%s?sslmode=verify-ca&connect_timeout=%d",
+			"postgres",
+			url.QueryEscape(c.Postgres.PostgresqlUser),
+			url.QueryEscape(c.Postgres.PostgresqlPassword),
+			c.Postgres.PostgresqlHost,
+			c.Postgres.PostgresqlPort,
+			c.Postgres.PostgresqlDbname,
+			5)
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
 
-	//Сконфигурируем пул, задав для него максимальное количество соединений
-	poolConfig, _ := pgxpool.ParseConfig(connStr)
-	poolConfig.MaxConns = 5
+		//Сконфигурируем пул, задав для него максимальное количество соединений
+		poolConfig, _ := pgxpool.ParseConfig(connStr)
+		poolConfig.MaxConns = 5
 
-	//Получаем пул соединений, используя контекст и конфиг
-	pool, err := pgxpool.ConnectConfig(ctx, poolConfig)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Connect to database failed: %v\n", err)
-		os.Exit(1)
+		//Получаем пул соединений, используя контекст и конфиг
+		pool, err := pgxpool.ConnectConfig(ctx, poolConfig)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Connect to database failed: %v\n", err)
+			os.Exit(1)
+		}
+
+		return pool, nil
+	} else {
+		connStr := fmt.Sprintf("%s://%s:%s@%s:%s/%s?sslmode=disable&connect_timeout=%d",
+			"postgres",
+			url.QueryEscape(c.Postgres.PostgresqlUser),
+			url.QueryEscape(c.Postgres.PostgresqlPassword),
+			c.Postgres.PostgresqlHost,
+			c.Postgres.PostgresqlPort,
+			c.Postgres.PostgresqlDbname,
+			5)
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		//Сконфигурируем пул, задав для него максимальное количество соединений
+		poolConfig, _ := pgxpool.ParseConfig(connStr)
+		poolConfig.MaxConns = 5
+
+		//Получаем пул соединений, используя контекст и конфиг
+		pool, err := pgxpool.ConnectConfig(ctx, poolConfig)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Connect to database failed: %v\n", err)
+			os.Exit(1)
+		}
+
+		return pool, nil
 	}
 
-	return pool, nil
 }
--- internal/db/redis/redis_conn.go
+++ internal/db/redis/redis_conn.go
@@ -3,22 +3,41 @@ package redis
 import (
 	"context"
 	"final-project/internal/config"
+	"os"
+
+	"crypto/tls"
 
 	"github.com/go-redis/redis/v8"
 )
 
 func NewRedisConn(c *config.Config) (*redis.Client, error) {
-	rdb := redis.NewClient(&redis.Options{
-		Addr: c.Redis.RedisHost + ":" + c.Redis.RedisPort,
-		//Username: c.Redis.RedisUsername,
-		Password: c.Redis.RedisPassword,
-		DB:       0,
-	})
-	ctx := context.Background()
-	_, err := rdb.Ping(ctx).Result()
-	if err != nil {
-		return nil, err
+	if os.Getenv("ENV") == "PRODUCTION" {
+		rdb := redis.NewClient(&redis.Options{
+			Addr: c.Redis.RedisHost + ":" + c.Redis.RedisPort,
+			//Username: c.Redis.RedisUsername,
+			Password: c.Redis.RedisPassword,
+			DB:       0,
+			TLSConfig: &tls.Config{
+				MinVersion: tls.VersionTLS12,
+			},
+		})
+		ctx := context.Background()
+		_, err := rdb.Ping(ctx).Result()
+		if err != nil {
+			return nil, err
+		}
+		return rdb, nil
+	} else {
+		rdb := redis.NewClient(&redis.Options{
+			Addr:     c.Redis.RedisHost + ":" + c.Redis.RedisPort,
+			Password: c.Redis.RedisPassword,
+			DB:       0,
+		})
+		ctx := context.Background()
+		_, err := rdb.Ping(ctx).Result()
+		if err != nil {
+			return nil, err
+		}
+		return rdb, nil
 	}
-
-	return rdb, nil
 }
