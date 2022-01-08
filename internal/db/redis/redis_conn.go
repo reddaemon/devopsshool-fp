f60f0dd70c072995bcb30cf5b5465ed1547752a6
reddaemon <steseagal87@gmail.com>
2022-01-08 19:01:18 +0300

    Redis conn (#19)
    * add values
    
    * made chart improvements
    
    * update chart and application
    
    * update .gitignore
    
    * update redis conn
    
    * fix redis conn problem

--- internal/db/redis/redis_conn.go
+++ internal/db/redis/redis_conn.go
@@ -11,8 +11,8 @@ import (
 func NewRedisConn(c *config.Config) (*redis.Client, error) {
 	if os.Getenv("ENV") == "PRODUCTION" {
 		rdb := redis.NewClient(&redis.Options{
-			Addr: c.Redis.RedisHost + ":" + c.Redis.RedisPort,
-			//Username: c.Redis.RedisUsername,
+			Addr:     c.Redis.RedisHost + ":" + c.Redis.RedisPort,
+			Username: c.Redis.RedisUsername,
 			Password: c.Redis.RedisPassword,
 			DB:       0,
 		})
